4bb6e6ba23aae3ca7408a4a53119c00b271dc368
MatMercer <contact@matbm.net>
2021-10-30 15:52:08 -0300

    Implement lang auto redirect in JS side

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,11 @@
 prettyWelcome = function (msg) {
-  console.log('%c        ' + msg + '        ', 'background: #111; color: yellow; font-weight: bold');
+  console.log('%c        ' + msg + '        ', 'background: #111; color: yellow; font-weight: bold; font-size: 24px');
+  console.log('%c    Parallel Cat Blog    ', 'background: #111; color: #9999CC; font-weight: bold; font-size: 16px');
 }
 
-switch (document.documentElement.lang) {
-  case 'pt':
-    prettyWelcome('Opa, parece que você abriu um console ;)');
-    break;
-  case 'en':
-    prettyWelcome('Oh, looks like you opened a console ;)');
-    break;
+welcomeMsgs = {
+  'pt': 'Opa, parece que você abriu um console :3',
+  'en': 'Oh, looks like you opened a console :3',
 }
 
+prettyWelcome(welcomeMsgs[document.documentElement.lang]);
